19b27d922018528b5b671952ca75977e4f97bb9c
Pawel Kochanski <pawilioxion@gmail.com>
2023-09-15 20:32:10 +0200

    added function to reduce repeated code

--- include/card_parser.h
+++ include/card_parser.h
@@ -7,6 +7,7 @@
 #include "drawing.h"
 #include "error_code.h"
 
-ErrorCode parse_card(card_entry** card, FILE card_data[static 1], SDL_Renderer* renderer);
+ErrorCode read_field(FILE card_data[static 1], const char* field_name, char* buffer, const char* buffer_start, const char* buffer_end);
+ErrorCode parse_card_data(card_entry** card, FILE card_data[static 1], SDL_Renderer* renderer);
 
 #endif
--- src/card_parser.c
+++ src/card_parser.c
@@ -1,10 +1,44 @@
+#include <ctype.h>
+
 #include "card_parser.h"
 #include "helpers.h"
 
-ErrorCode parse_card(card_entry** card, FILE card_data[static 1], SDL_Renderer* renderer)
+ErrorCode read_field(FILE card_data[static 1], const char* field_name, char* buffer, const char* buffer_start, const char* buffer_end)
+{
+    int c;
+    size_t offset = 0;
+
+    while(isspace(c = getc(card_data))){}
+
+    ungetc(c, card_data);
+
+    if(read_until(card_data, buffer, ':', 0) != ERR_OK)
+        return ERR_FILE_FORMAT;
+
+    if(strncmp(buffer, field_name, MAX_NAME_LEN) != 0)
+        return ERR_FILE_FORMAT;
+
+    fgetc(card_data);
+
+    if(buffer_start)
+    {
+        snprintf(buffer, MAX_NAME_LEN, buffer_start);
+
+        offset = strlen(buffer);
+    }
+
+    if(read_until(card_data, buffer, '\n', offset) != ERR_OK)
+        return ERR_FILE_FORMAT;
+
+    if(buffer_end)
+        strncat(buffer, buffer_end, MAX_NAME_LEN - strlen(buffer));
+
+    return ERR_OK;
+}
+
+ErrorCode parse_card_data(card_entry** card, FILE card_data[static 1], SDL_Renderer* renderer)
 {
     char buffer[MAX_NAME_LEN];
-    size_t offset;
 
     card_entry* to_add = malloc(sizeof(card_entry));
 
@@ -13,39 +47,17 @@ ErrorCode parse_card(card_entry** card, FILE card_data[static 1], SDL_Renderer* 
     if(strncmp(buffer, "{\n", MAX_NAME_LEN) != 0)
         return ERR_FILE_FORMAT;
 
-    if(read_until(card_data, buffer, ':', 0) != ERR_OK)
+    if(read_field(card_data, "file_name", buffer, "assets/cards/", ".bmp") != ERR_OK)
         return ERR_FILE_FORMAT;
-
-    if(strncmp(buffer, "    file_name", MAX_NAME_LEN) != 0)
-        return ERR_FILE_FORMAT;
-
-    fgetc(card_data);
-
-    snprintf(buffer, MAX_NAME_LEN, "cards/");
-
-    offset = strlen(buffer);
-
-    if(read_until(card_data, buffer, '\n', offset) != ERR_OK)
-        return ERR_FILE_FORMAT;
-
-    strncat(buffer, ".bmp", MAX_NAME_LEN - strlen(buffer));
 
     SDL_Texture* texture;
 
-    if(load_card(&texture, renderer, buffer) != ERR_OK)
+    if(load_texture(&texture, renderer, buffer) != ERR_OK)
         return ERR_FILE_FORMAT;
 
     to_add->texture = texture;
 
-    if(read_until(card_data, buffer, ':', 0) != ERR_OK)
-        return ERR_FILE_FORMAT;
-
-    if(strncmp(buffer, "    card_name", MAX_NAME_LEN) != 0)
-        return ERR_FILE_FORMAT;
-
-    fgetc(card_data);
-
-    if(fgets(buffer, MAX_NAME_LEN, card_data) == NULL)
+    if(read_field(card_data, "card_name", buffer, NULL, NULL) != ERR_OK)
         return ERR_FILE_FORMAT;
 
     strcpy(to_add->name, buffer);
